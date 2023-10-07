d7039c898da1e1dc80e91f01c505c3a445a1bfb8
snow ruteka <136928692+baphotex@users.noreply.github.com>
2023-10-07 01:23:30 -0500

    Update nyx.py

--- helper/nyx.py
+++ helper/nyx.py
@@ -14,6 +14,4 @@ c_vecs = lambda X:np.array([X[:,i].reshape(-1,1) for i in range(0,len(X.T))])
 getV = lambda X: np.concatenate(normalize(get_vecs(X)),axis=1)
 
 _formU = lambda i,v: (np.arange(len(X))==i).astype(int).reshape(-1,1)*np.linalg.norm(v) 
-getU = lambda X: np.concatenate(
-    [_formU(i,v) for (i,v) in enumerate(c_vecs(X))],axis=1
-)
+getU = lambda X: np.concatenate([_formU(i,v) for (i,v) in enumerate(get_vecs(X))],axis=1)
